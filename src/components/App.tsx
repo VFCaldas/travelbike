17a4db0d731b5d0a83bb38a7cb66bda13b23e8b8
Douglasfj <douglasfj2004@gmail.com>
2023-07-08 11:09:03 -0300

    Alterando projeto de javascript para typescript

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+// import {  } from "axios"; // Estudar como funciona a biblioteca axios (é uma biblioteca de HttpRequests)
+
+function App() {
+  return (
+    <>
+      <h1>TRAVEL BIKE</h1>
+    </>
+  )
+}
+
+export default App
