c7636f9a6adc611d299bc0ac01e23660dd8d2075
Douglasfj <douglasfj2004@gmail.com>
2023-07-10 18:20:34 -0300

    iniciando implementação de rotas

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,9 @@
-import { useState } from 'react'
-import { DatabaseService } from '../services/DatabaseService'
-
 import './App.css'
+import RoutesComponent from './routes/RoutesComponent'
 
 function App() {
-  const [texto, setTexto] = useState("OI");
-
-  // DatabaseService.insert({ 
-  //   teste: 1,
-  //   teste2: "ASDJAKSDJLK"
-  // }, "teste/")
-
-  let response = DatabaseService.select("teste/")
-  response.then((value)=>{
-    setTexto(JSON.stringify(value.toJSON()))
-  });
-
-  return (
-    <>
-      <h1>TRAVEL BIKE</h1>
-      <p>{texto}</p>
-    </>
+  return(
+    <RoutesComponent/>
   )
 }
 
